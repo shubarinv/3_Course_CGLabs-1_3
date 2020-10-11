de2bae111d646ba97148d647354e8492dbad84a7
Vladimir Shubarin <vhundef@gmail.com>
2020-10-11 21:55:08 +0300

    Added glew init

--- Application.cpp
+++ Application.cpp
@@ -1,8 +1,11 @@
 #define GL_SILENCE_DEPRECATION
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <GLUT/glut.h>
+
 #include <spdlog/spdlog.h>
 
-int main() {
+int main(int argc, char *argv[]) {
   spdlog::info("App stated!");
   GLFWwindow *window;
 
@@ -11,6 +14,11 @@ int main() {
 	spdlog::critical("Application::Failed to init GLFW");
 	throw std::runtime_error("Failed to init GLFW");
   }
+  glfwWindowHint(GLFW_SAMPLES, 4);              // 4x antialiasing
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);// We want OpenGL 4.1
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);          // To make MacOS happy; should not be needed
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);// We don't want the old OpenGL
   spdlog::debug("GLFW Init - OK");
   /* Create a windowed mode window and its OpenGL context */
   window = glfwCreateWindow(640, 480, "Hello World", nullptr, nullptr);
@@ -23,6 +31,25 @@ int main() {
 
   /* Make the window's context current */
   glfwMakeContextCurrent(window);
+  GLenum err = glewInit();
+  if (GLEW_OK != err) {
+	/* Problem: glewInit failed, something is seriously wrong. */
+	spdlog::critical("Application::GLEW init failed: {}", glewGetErrorString(err));
+  }
+  spdlog::info("Status: Using GLEW v{}", glewGetString(GLEW_VERSION));
+  spdlog::info("Status: Using OpenGL v{}", glGetString(GL_VERSION));
+
+  float positions[] = {
+	  -0.5f, -0.5f,
+	  0.5f, 0.f,
+	  0.5f, 0.5f};
+  unsigned int buffer;
+  glGenBuffers(1, &buffer);
+  glBindBuffer(GL_ARRAY_BUFFER, buffer);
+  glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+  glEnableVertexAttribArray(0);
+  glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, nullptr);
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
 
   /* Loop until the user closes the window */
   while (!glfwWindowShouldClose(window)) {
