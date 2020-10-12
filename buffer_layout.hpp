09981b25c9c2aa42c3de6e460d5d3bdd9d67adf0
Vladimir Shubarin <vhundef@gmail.com>
2020-10-12 21:24:08 +0300

    Added bufferLayout class

---
+++ buffer_layout.hpp
@@ -0,0 +1,11 @@
+//
+// Created by Vladimir Shubarin on 12.10.2020.
+//
+
+#ifndef CGLABS__BUFFER_LAYOUT_HPP_
+#define CGLABS__BUFFER_LAYOUT_HPP_
+
+class BufferLayout {
+};
+
+#endif//CGLABS__BUFFER_LAYOUT_HPP_
