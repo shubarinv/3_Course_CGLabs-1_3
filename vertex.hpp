ea9d51f047e6c2244e6f905bd34b7d7a12a006f4
Vladimir Shubarin <vhundef@gmail.com>
2020-10-13 09:41:02 +0300

    Added vertex class

---
+++ vertex.hpp
@@ -0,0 +1,49 @@
+//
+// Created by Vladimir Shubarin on 10/13/20.
+//
+
+#ifndef CGLABS__VERTEX_HPP_
+#define CGLABS__VERTEX_HPP_
+#include "renderer.hpp"
+class Vertex {
+  glm::vec3 position{};
+ public:
+  [[maybe_unused]] [[nodiscard]] const glm::vec3 &getPosition() const {
+	return position;
+  }
+  [[maybe_unused]] [[nodiscard]] const glm::vec3 &getColor() const {
+	return color;
+  }
+  explicit Vertex(glm::vec3 _pos = {0, 0, 0}, glm::vec3 _color = {0, 0, 0}) {
+	setPosition(_pos);
+	setColor(_color);
+  }
+ private:
+  glm::vec3 color{};
+  void setPosition(glm::vec3 _newPosition) {
+	if (_newPosition.x < -1 || _newPosition.x > 1 ||
+		_newPosition.y < -1 || _newPosition.y > 1 ||
+		_newPosition.z < -1 || _newPosition.z > 1) {
+	  spdlog::error("Invalid location!");
+	  throw std::runtime_error("Invalid location for vertex");
+	}
+	position = _newPosition;
+  }
+  void setColor(glm::vec3 _color) {
+	if (_color.x < -1 || _color.x > 1 ||
+		_color.y < -1 || _color.y > 1 ||
+		_color.z < -1 || _color.z > 1) {
+	  spdlog::error("Invalid color!");
+	}
+	color = _color;
+  }
+  bool operator==(const Vertex &b) const {
+	if (position.x == b.position.x &&
+		position.y == b.position.y &&
+		position.z == b.position.z) {
+	  return true;
+	} else return false;
+  }
+};
+
+#endif//CGLABS__VERTEX_HPP_
