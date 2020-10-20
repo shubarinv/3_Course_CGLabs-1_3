9619623076dda9192f8c3d24dce7b419163a02d3
Vladimir Shubarin <vhundef@gmail.com>
2020-10-20 22:02:28 +0300

    Shaders now suport MVP

--- resources/shaders/basic_w_uniforms.glsl
+++ resources/shaders/basic_w_uniforms.glsl
@@ -2,9 +2,9 @@
 #version 410 core
 
 layout(location=0)in vec4 position;
-
+uniform mat4 u_MVP;
 void main(){
-    gl_Position =position;
+    gl_Position =u_MVP*position;
 }
 
     #shader fragment
