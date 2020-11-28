41cc112024be852e6c369926e0cfca8e213c0b89
Vladimir Shubarin <vhundef@gmail.com>
2020-11-28 22:41:58 +0300

    Added docs for vertex.hpp

--- vertex.hpp
+++ vertex.hpp
@@ -5,21 +5,26 @@
 #ifndef CGLABS__VERTEX_HPP_
 #define CGLABS__VERTEX_HPP_
 class Vertex {
-  glm::vec3 position{};
-  bool front = true;
+  glm::vec3 position{}; ///< @brief coords of Vertex
+  [[deprecated]]bool front = true; ///<@deprecated this value is not used and will be removed in the future
 
  public:
-  [[maybe_unused]] [[nodiscard]] bool isFront() const {
-	return front;
-  }
-
- public:
+  /**
+   * @returns returns position of the Vertex
+   */
   [[maybe_unused]] [[nodiscard]] const glm::vec3 &getPosition() const {
 	return position;
   }
+  /**
+   * @returns returns color of the vertex
+   */
   [[maybe_unused]] [[nodiscard]] const glm::vec3 &getColor() const {
 	return color;
   }
+  /**
+   * @param _pos vec3 position of vertex
+   * @param _color vec3 color of vertex
+   */
   explicit Vertex(glm::vec3 _pos = {0, 0, 0}, glm::vec3 _color = {0, 0, 0}) {
 	setPosition(_pos);
 	setColor(_color);
@@ -27,6 +32,10 @@ class Vertex {
 
  private:
   glm::vec3 color{};
+  /**
+   * @brief sets position for the vertex
+   * @param _newPosition new position for the vertex
+   */
   void setPosition(glm::vec3 _newPosition) {
 	if (_newPosition.x < -1 || _newPosition.x > 1 || _newPosition.y < -1 || _newPosition.y > 1 || _newPosition.z < -1 || _newPosition.z > 1) {
 	  spdlog::error("Invalid location!");
@@ -34,12 +43,20 @@ class Vertex {
 	}
 	position = _newPosition;
   }
+  /**
+   * @brief sets color for vertex
+   * @param _color vec3 color for vertex
+   */
   void setColor(glm::vec3 _color) {
 	if (_color.x < -1 || _color.x > 1 || _color.y < -1 || _color.y > 1 || _color.z < -1 || _color.z > 1) {
 	  spdlog::error("Invalid color!");
 	}
 	color = _color;
   }
+
+  /**
+   * @brief Compares two vertices
+   */
   bool operator==(const Vertex &b) const {
 	if (position.x == b.position.x && position.y == b.position.y && position.z == b.position.z) {
 	  return true;
