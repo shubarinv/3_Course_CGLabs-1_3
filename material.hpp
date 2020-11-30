23a27b56d72f3ceea5d93dbb5c5193c1a47b8e5f
Vladimir Shubarin <vhundef@gmail.com>
2020-11-30 21:30:18 +0300

    Added constructor for Material class

--- material.hpp
+++ material.hpp
@@ -11,10 +11,10 @@ class Material {
   glm::vec3 specular{};
   float shininess{0};
   Material(glm::vec3 _ambient, glm::vec3 _diffuse, glm::vec3 _specular, float _shininess) {
-  ambient = _ambient;
-  diffuse = _diffuse;
-  specular = _specular;
-  shininess = _shininess;
+	ambient = _ambient;
+	diffuse = _diffuse;
+	specular = _specular;
+	shininess = _shininess;
   }
 };
 
