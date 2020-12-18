b740ac7081a28171254c218db82add144e929569
Vladimir Shubarin <vhundef@gmail.com>
2020-12-18 20:43:26 +0300

    Fixed camera and now it works

--- camera.hpp
+++ camera.hpp
@@ -10,7 +10,8 @@ class Camera {
   glm::vec3 lookingAt{};
   glm::mat4 projection{};
   glm::mat4 model{};
-glm::mat4 view;
+  glm::mat4 view;
+
  public:
   Camera() {
 	projection = glm::perspective(
@@ -19,6 +20,7 @@ glm::mat4 view;
 		0.1f,              // Ближняя плоскость отсечения. Должна быть больше 0.
 		100.0f             // Дальняя плоскость отсечения.
 	);
+	model = glm::mat4(1.0f);
   }
 
   explicit Camera(glm::vec2 windowSize) {
@@ -28,30 +30,40 @@ glm::mat4 view;
 		0.1f,                       // Ближняя плоскость отсечения. Должна быть больше 0.
 		100.0f                      // Дальняя плоскость отсечения.
 	);
+	model = glm::mat4(1.0f);
   }
   void lookAt(glm::vec3 lookAt) {
 	lookingAt = lookAt;
+	updateView();
   }
   void moveTo(glm::vec3 moveTo) {
 	position = moveTo;
+	updateView();
   }
-  void setModel(glm::mat4 _model){
-	model=_model;
+  void setModel(glm::mat4 _model) {
+	model = _model;
   }
-  glm::mat4 getModel(){
-	  return model;
+  glm::mat4 getModel() {
+	return model;
   }
-  glm::mat4 getView(){
-	view=glm::lookAt(
-		position,// Камера находится в мировых координатах (4,3,3)
-		lookingAt,// И направлена в начало координат
-		glm::vec3(0, 1, 0) // "Голова" находится сверху
-	);
+  glm::mat4 getProjection() {
+	return projection;
+  }
+  glm::mat4 getView() {
+	view = glm::lookAt(
+		position,
+		lookingAt,
+		glm::vec3(0, 1, 0));
 	return view;
   }
-  glm::mat4 getMVP(){
-	auto model = glm::mat4(1.0f);
-	return  projection * view * model;
+  glm::mat4 getMVP() {
+	return projection * view * model;
+  }
+  void updateView() {
+	view = glm::lookAt(
+		position,
+		lookingAt,
+		glm::vec3(0, 1, 0));
   }
 };
 
