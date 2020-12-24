a3947a5e9a803a8367735568320531a2fcc1d2eb
Vladimir Shubarin <vhundef@gmail.com>
2020-12-24 13:05:56 +0300

    Lab4 task 3 Done

--- Labs/lab4.cpp
+++ Labs/lab4.cpp
@@ -17,6 +17,7 @@ using namespace Magnum;
 #include <Magnum/GL/Buffer.h>
 #include <Magnum/GL/Mesh.h>
 #include <Magnum/Math/Color.h>
+#include <Magnum/Primitives/Cone.h>
 #include <Magnum/Primitives/UVSphere.h>
 
 #include "../functions.hpp"
@@ -33,18 +34,31 @@ class lab4 : public Platform::Application {
 	GL::Renderer::enable(GL::Renderer::Feature::DepthTest);
 	GL::Renderer::enable(GL::Renderer::Feature::FaceCulling);
 	lightCoords = getCoordsForLight(0, 0, 4, 400);
+
 	Trade::MeshData sphere = Primitives::uvSphereSolid(20, 20);
+	Trade::MeshData pyramid = Primitives::coneSolid(20, 20, 1.f);
 
-	GL::Buffer vertices;
-	vertices.setData(MeshTools::interleave(sphere.positions3DAsArray(), sphere.normalsAsArray()));
-	std::pair<Containers::Array<char>, MeshIndexType> compressed = MeshTools::compressIndices(sphere.indicesAsArray());
-	GL::Buffer indices;
-	indices.setData(compressed.first);
+	GL::Buffer vertices_sphere;
+	vertices_sphere.setData(MeshTools::interleave(sphere.positions3DAsArray(), sphere.normalsAsArray()));
+	std::pair<Containers::Array<char>, MeshIndexType> compressed_sphere = MeshTools::compressIndices(sphere.indicesAsArray());
+	GL::Buffer indices_sphere;
+	indices_sphere.setData(compressed_sphere.first);
 
-	mesh.setPrimitive(sphere.primitive())
+	GL::Buffer vertices_pyramid;
+	vertices_pyramid.setData(MeshTools::interleave(pyramid.positions3DAsArray(), pyramid.normalsAsArray()));
+	std::pair<Containers::Array<char>, MeshIndexType> compressed_pyramid = MeshTools::compressIndices(pyramid.indicesAsArray());
+	GL::Buffer indices_pyramid;
+	indices_pyramid.setData(compressed_pyramid.first);
+
+	mesh_sphere.setPrimitive(sphere.primitive())
 		.setCount(sphere.indexCount())
-		.addVertexBuffer(std::move(vertices), 0, Shaders::Phong::Position{}, Shaders::Phong::Normal{})
-		.setIndexBuffer(std::move(indices), 0, compressed.second);
+		.addVertexBuffer(std::move(vertices_sphere), 0, Shaders::Phong::Position{}, Shaders::Phong::Normal{})
+		.setIndexBuffer(std::move(indices_sphere), 0, compressed_sphere.second);
+
+	mesh_pyramid.setPrimitive(pyramid.primitive())
+		.setCount(pyramid.indexCount())
+		.addVertexBuffer(std::move(vertices_pyramid), 0, Shaders::Phong::Position{}, Shaders::Phong::Normal{})
+		.setIndexBuffer(std::move(indices_pyramid), 0, compressed_pyramid.second);
 
 	transformation =
 		Matrix4::rotationX(Math::Deg(30.0f)) * Matrix4::rotationY(Math::Deg(40.0f));
@@ -55,6 +69,8 @@ class lab4 : public Platform::Application {
 	color = Color3::fromHsv({Math::Deg(35.0f), 1.0f, 1.0f});
 
 	lights.addLight({{0, 0, 10}, {0.8, 0.8, 0.8}, "t1_1"});
+	lights.addLight({{10, 0, 0}, {0.8, 0, 0.8}, "t3_1"});
+	lights.addLight({{-10, 0, 10}, {0.8, 0.8, 0}, "t3_2"});
 	shader = Shaders::Phong({}, lights.countEnabledLights());
   }
 
@@ -63,20 +79,37 @@ class lab4 : public Platform::Application {
   void drawEvent() override {
 	GL::defaultFramebuffer.clear(GL::FramebufferClear::Color | GL::FramebufferClear::Depth);
 
+	if (selectedTask == 0) {
+	  lights.lookForTheLight("t1_1")->enable();
+	  lights.lookForTheLight("t3_1")->disable();
+	  lights.lookForTheLight("t3_2")->disable();
+	  shader.draw(mesh_sphere);
+	}
+	if (selectedTask == 1) {
+	  lights.lookForTheLight("t1_1")->enable();
+	  lights.lookForTheLight("t1_1")->color = {0.8f, 0.8f, 0.8f};
+	  lights.lookForTheLight("t3_1")->disable();
+	  lights.lookForTheLight("t3_2")->disable();
+	  shader.draw(mesh_sphere);
+	}
+	if (selectedTask == 2) {
+	  lights.lookForTheLight("t1_1")->disable();
+	  lights.lookForTheLight("t3_1")->enable();
+	  lights.lookForTheLight("t3_2")->enable();
+	  shader.draw(mesh_pyramid);
+	}
+
 	if (shader.lightCount() != lights.countEnabledLights()) {
 	  shader = Shaders::Phong({}, lights.countEnabledLights());
 	}
-	if(selectedTask==1){
-	  lights.lookForTheLight("t1_1")->color={0.8f,0.8f,0.8f};
-	}
+
 	shader.setLightPositions(lights.getPositions())
 		.setLightColors(lights.getColors())
 		.setDiffuseColor({1, 1, 1})
 		.setAmbientColor({0.1, 0.1, 0.1})
 		.setTransformationMatrix(transformation)
 		.setNormalMatrix(transformation.normalMatrix())
-		.setProjectionMatrix(projection)
-		.draw(mesh);
+		.setProjectionMatrix(projection);
 	currentLightPosition++;
 	if (currentLightPosition > lightCoords.size()) {
 	  currentLightPosition = 0;
@@ -85,7 +118,8 @@ class lab4 : public Platform::Application {
 	redraw();
   }
 
-  GL::Mesh mesh;
+  GL::Mesh mesh_sphere;
+  GL::Mesh mesh_pyramid;
   Shaders::Phong shader;
   Matrix4 transformation, projection;
   Color3 color;
@@ -122,23 +156,27 @@ class lab4 : public Platform::Application {
 	}
 	if (event.key() == Key::Right) {
 	  selectedTask++;
-	  if (selectedTask > 8) {
+	  if (selectedTask > 3) {
 		selectedTask = 0;
 	  }
 	  LOG_S(INFO) << "Selected task: " << selectedTask;
 	}
-	if (event.key() == Key::W) {if (selectedTask == 1) {
-		lights.lookForTheLight("t1_1")->position = {lights.lookForTheLight("t1_1")->position.x(), lights.lookForTheLight("t1_1")->position.y(), lights.lookForTheLight("t1_1")->position.z()- 0.5f};
-	  }}
-	if (event.key() == Key::S) {if (selectedTask == 1) {
-		lights.lookForTheLight("t1_1")->position = {lights.lookForTheLight("t1_1")->position.x(), lights.lookForTheLight("t1_1")->position.y(), lights.lookForTheLight("t1_1")->position.z()+ 0.5f};
-	  }}
+	if (event.key() == Key::W) {
+	  if (selectedTask == 1) {
+		lights.lookForTheLight("t1_1")->position = {lights.lookForTheLight("t1_1")->position.x(), lights.lookForTheLight("t1_1")->position.y(), lights.lookForTheLight("t1_1")->position.z() - 0.5f};
+	  }
+	}
+	if (event.key() == Key::S) {
+	  if (selectedTask == 1) {
+		lights.lookForTheLight("t1_1")->position = {lights.lookForTheLight("t1_1")->position.x(), lights.lookForTheLight("t1_1")->position.y(), lights.lookForTheLight("t1_1")->position.z() + 0.5f};
+	  }
+	}
 	if (event.key() == Key::A) {
 	  if (selectedTask == 0) {
 		lights.lookForTheLight("t1_1")->color = {lights.lookForTheLight("t1_1")->color.rgb().r(), 0, lights.lookForTheLight("t1_1")->color.rgb().b() - 0.05f};
 	  }
 	  if (selectedTask == 1) {
-		lights.lookForTheLight("t1_1")->position = {lights.lookForTheLight("t1_1")->position.x()- 0.5f, lights.lookForTheLight("t1_1")->position.y(), 10};
+		lights.lookForTheLight("t1_1")->position = {lights.lookForTheLight("t1_1")->position.x() - 0.5f, lights.lookForTheLight("t1_1")->position.y(), 10};
 	  }
 	}
 	if (event.key() == Key::D) {
@@ -146,7 +184,41 @@ class lab4 : public Platform::Application {
 		lights.lookForTheLight("t1_1")->color = {lights.lookForTheLight("t1_1")->color.rgb().r(), 0, lights.lookForTheLight("t1_1")->color.rgb().b() + 0.05f};
 	  }
 	  if (selectedTask == 1) {
-		lights.lookForTheLight("t1_1")->position = {lights.lookForTheLight("t1_1")->position.x()+ 0.5f, lights.lookForTheLight("t1_1")->position.y(), 10};
+		lights.lookForTheLight("t1_1")->position = {lights.lookForTheLight("t1_1")->position.x() + 0.5f, lights.lookForTheLight("t1_1")->position.y(), 10};
+	  }
+	}
+	if (event.key() == Key::Y) {
+	  if (selectedTask == 2) {
+		lights.lookForTheLight("t3_1")->color =
+			{lights.lookForTheLight("t3_1")->color.rgb().r() - 0.05f,
+			 0,
+			 lights.lookForTheLight("t3_1")->color.rgb().b() - 0.05f};
+		lights.lookForTheLight("t3_2")->color =
+			{lights.lookForTheLight("t3_2")->color.rgb().r() - 0.05f,
+			 lights.lookForTheLight("t3_2")->color.rgb().g() - 0.05f,
+			 0};
+	  }
+	}
+	if (event.key() == Key::P) {
+	  if (selectedTask == 2) {
+		lights.lookForTheLight("t3_1")->color =
+			{lights.lookForTheLight("t3_1")->color.rgb().r() + 0.05f,
+			 0,
+			 lights.lookForTheLight("t3_1")->color.rgb().b() + 0.05f};
+		lights.lookForTheLight("t3_2")->color =
+			{lights.lookForTheLight("t3_2")->color.rgb().r() + 0.05f,
+			 lights.lookForTheLight("t3_2")->color.rgb().g() + 0.05f,
+			 0};
+	  }
+	}
+	if (selectedTask == 2) {
+	  if (lights.lookForTheLight("t3_1")->color.rgb().r() >= 1) {
+		lights.lookForTheLight("t3_1")->color = {0.1, 0, 0.1};
+		lights.lookForTheLight("t3_2")->color = {0.1, 0.1, 0};
+	  }
+	  if (lights.lookForTheLight("t3_1")->color.rgb().r() <= 0) {
+		lights.lookForTheLight("t3_1")->color = {0, 0, 0};
+		lights.lookForTheLight("t3_2")->color = {0, 0, 0};
 	  }
 	}
   }
