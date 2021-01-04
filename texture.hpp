592ef6f77edf18599490d7460a40a78e79bd1dfc
Vladimir Shubarin <vhundef@gmail.com>
2021-01-04 10:03:57 +0300

    texture class should now load specified texture

--- texture.hpp
+++ texture.hpp
@@ -17,17 +17,16 @@ class Texture {
 	Containers::Pointer<Trade::AbstractImporter> importer =manager.loadAndInstantiate("AnyImageImporter");
 	if (!importer) std::exit(1);
 
-	if (!importer->openFile("/path/to/myfile.jpg"))
+	if (!importer->openFile(filepath))
 	  std::exit(2);
 	/* Set texture data and parameters */
-	Containers::Optional<Trade::ImageData2D> image = importer->image2D(0);
+	auto image = importer->image2D(0);
 	CORRADE_INTERNAL_ASSERT(image);
 	_texture.setWrapping(GL::SamplerWrapping::ClampToEdge)
 		.setMagnificationFilter(GL::SamplerFilter::Linear)
 		.setMinificationFilter(GL::SamplerFilter::Linear)
 		.setStorage(1, GL::textureFormat(image->format()), image->size())
 		.setSubImage(0, {}, *image);
-	Containers::Optional<Trade::ImageData2D> image = importer->image2D(0);
 	CORRADE_INTERNAL_ASSERT(image);
   }
 };
